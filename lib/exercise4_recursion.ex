9ef73484e99c12dfa1386bd9fa768aa3ff02c931
Andrea Fuentes <andreita_f92@hotmail.com>
2019-03-19 00:17:58 -0500

    Excercise of recursion - improvement to not integer item validation

--- lib/exercise4_recursion.ex
+++ lib/exercise4_recursion.ex
@@ -1,6 +1,6 @@
 defmodule ElixirExercises.Exercise4Recursion do
-  def list_sum([h | t]) do
-    list_sum(t, h)
+  def list_sum([_h | _t] = list) do
+    list_sum(list, 0, 0, %{})
   end
 
   def list_sum([]) do
@@ -11,14 +11,21 @@ defmodule ElixirExercises.Exercise4Recursion do
     {:error, "It isn't a stupid list, think please xD"}
   end
 
-  defp list_sum([], sum) do
-    {:ok, sum}
+  defp list_sum([], sum, _counter, error_map) do
+    case Map.keys(error_map) do
+      [] -> {:ok, sum}
+      _ -> {:error, error_map}
+    end
   end
 
-  defp list_sum([h | t], sum) do
+  defp list_sum([h | t], sum, counter, error_map) do
     cond do
-      is_integer(h) -> list_sum(t, sum + h)
-      true -> {:error, "There is a not integer item in the list"}
+      is_integer(h) ->
+        list_sum(t, sum + h, counter + 1, error_map)
+
+      true ->
+        error_map = Map.put(error_map, counter, h)
+        list_sum(t, sum, counter + 1, error_map)
     end
   end
 end
